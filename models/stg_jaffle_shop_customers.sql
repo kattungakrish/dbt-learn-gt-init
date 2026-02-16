ebb2e383895eeffbbf8fa7eaea90ab015348929c
Krishi Kattunga <kattungakrish@gmail.com>
2026-02-15 22:40:12 -0500

    delete duplicate models

--- models/stg_jaffle_shop_customers.sql
+++
@@ -1,8 +0,0 @@
-    --File Naming convention: <type of model- stg = staging model>_<schema_name>_<name_of_dataset>.sql
-    
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
